8c97a226999de314a8a6708fcc6d3c8efe18722c
Adam Powell <43593885+adamtpowell@users.noreply.github.com>
2019-10-19 14:29:21 -0400

    Initial homepage and api endpoints done

--- spider/index.js
+++ spider/index.js
@@ -1,26 +1,14 @@
-const puppeteer = require('puppeteer');
-let browser, page;
-
-puppeteer.launch().then((newBrowser)=>{
-    browser = newBrowser;
-    browser.newPage().then((newPage)=> {
-        page = newPage;
-    })
-});
 
 let express = require('express')
 let app = express()
 
+let renderer = require('./puppeteerApi')
+
+
 // Api endpoint to get the complete content of a page.
 app.get('/getpagecontent', async (req, res) => {
     try {
-        console.log(req.query.url)
-        await page.goto(req.query.url);
-
-        // Get body content.
-        let body = await page.$("body");
-        let content = await page.evaluate(element => element.innerText, body);
-        res.send(content);
+        res.send(await renderer.getContent(req.query.url));
     } catch (e) {
         res.send(e);
     }
@@ -29,13 +17,7 @@ app.get('/getpagecontent', async (req, res) => {
 // Api endpoint to get a base64 encoded screenshot of a page
 app.get('/getpagescreenshot', async (req, res) => {
     try {
-        console.log(req.query.url)
-        await page.goto(req.query.url);
-
-        // save screenshot
-        await page.setViewport({height:1080, width:1920})
-        let screenshot = await page.screenshot({ encoding: 'base64'});
-
+        let screenshot = await renderer.getScreenshot(req.query.url);
         if (req.query.pretty != null) {
             res.send(`<img src="data:image/png;base64, ${screenshot}" />`);
         } else {
@@ -45,5 +27,7 @@ app.get('/getpagescreenshot', async (req, res) => {
         res.send(e);
     }
 })
+
+app.use(express.static("static"));
 
 app.listen(3000)

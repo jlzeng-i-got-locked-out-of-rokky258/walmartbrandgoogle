b61a3c0a1a90d6560fe2fcc3ef95c2b335923175
Jerry Zeng <rokky258@gmail.com>
2019-10-20 02:09:05 -0400

    Drank juice tonight it was good

--- spider/juice.js
+++ spider/juice.js
@@ -15,23 +15,42 @@ connection.query('SELECT 1', function (error, results, fields) {
     if (error) throw error;
     // connected!
 });
-
+let arrayofUrlthenCount;
 let searchTerm = "hack";
 let searchWords = searchTerm.split(" ");
 
+function sortCount(a, b) {
+    return a.count - b.count;
+}
+
 async function searchAll(searchWords) {
-    for (let i = 0; i < urls.length; ++i) {
+    for (let i = 0; i < searchWords.length; ++i) {
         searchWords[i] = searchWords[i].trim();
         await searcher(searchWords[i]);
+        arrayofUrlthenCount.sort(sortCount);
     }
 }
 
-searchAll(searchWords).then(res=>console.log("Parse all done"));
+searchAll(searchWords).then(res=>console.log("Search all done"));
 
 
 async function searcher(word){
     try {
-
+        connection.query(`SELECT (SELECT url FROM documents WHERE id = words.document) AS url, words.count FROM words WHERE word = "${word}";`, async function (error, results, fields) {
+            if (error) throw error;
+           for(let x = 0; x < results.length; ++x){
+               for(let y = 0; y <= arrayofUrlthenCount.size; ++y){
+                   if(y === arrayofUrlthenCount.size){
+                       arrayofUrlthenCount.push(results[x]);
+                   }
+                   else{
+                       if(arrayofUrlthenCount[y].url === results[x].url){
+                           arrayofUrlthenCount[y].count += results[x].count;
+                       }
+                   }
+               }
+           }
+        })
     } catch (e) {
         console.log(e);
     }
